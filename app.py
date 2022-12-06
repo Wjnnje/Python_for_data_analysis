a88cb6f302ac433f9e3d7dab7d14c85321badf4a
Nayru12 <71485696+Nayru12@users.noreply.github.com>
2022-12-06 18:26:13 +0100

    adding API files

--- app.py
+++ app.py
@@ -17,16 +17,14 @@ def predict():
 	humidity = float(request.form['humidity'])
 	wind_speed = float(request.form['wind_speed'])
 	visibility = float(request.form['visibility'])
-	dew_point_temperature = float(request.form['dew_point_temperature'])
 	solar_radiation = float(request.form['solar_radiation'])
 	rainfall = float(request.form['rainfall'])
 	snowfall = float(request.form['snowfall'])
-	seasons = int(request.form['seasons'])
 	holiday = int(request.form['holiday'])
 	day = int(request.form['day'])
 	month = int(request.form['month'])
 	
-	prediction = model.predict([[hour, temperature, humidity, wind_speed, visibility, dew_point_temperature, solar_radiation, rainfall, snowfall, seasons, holiday, day, month]])
+	prediction = model.predict([[hour, temperature, humidity, wind_speed, visibility, solar_radiation, rainfall, snowfall, holiday, day, month]])
 	output =  int(prediction) if prediction >=0 else 0
 	#output = round(prediction[0], 2)
 	return render_template('index.html', prediction_text=f'Number of rented bikes: {output}')
